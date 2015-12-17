bf4b66ee1654541b36baca9a2c50cdac23206371
fafrd <kbradley@ucmerced.edu>
2015-12-16 17:20:06 -0800

    fixed

--- turdle/initialize.sh
+++ turdle/initialize.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 echo "Setting up turdle webapp in Docker. requires git, docker, docker-compose to be installed."
-
-echo "Cloning turdle ..."
 git clone https://github.com/fafrd/turdle
-
-echo "Building docker images ..."
 docker-compose up -d
